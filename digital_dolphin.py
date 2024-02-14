9cf2406ed7339e7359284f794ee1e9080eb9909a
Grant Eagon <grant.eagon@gmail.com>
2024-02-14 11:39:38 -0500

    add command line interface

--- digital_dolphin.py
+++ digital_dolphin.py
@@ -80,21 +80,111 @@ def speak_dolphin(dolphin_text):
     os.system("afplay dolphin.mp3")
 
 
+def command_line_interface():
+    """Command line interface."""
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Text to binary to dolphin converter.")
+    # all arguments are optional
+    parser.add_argument("--encode", help="Text to convert to dolphin.", required=False)
+    parser.add_argument(
+        "--speak",
+        help="Speak dolphin? (y/n): (pip install pydub pyrubberband soundfile gtts)",
+        required=False,
+    )
+    parser.add_argument(
+        "--copy",
+        help="Copy to clipboard? (y/n): (pip install pyperclip)",
+        required=False,
+    )
+    parser.add_argument("--decode", help="Decode dolphin to text.", required=False)
+    args = parser.parse_args()
+    if args.encode:
+        binary = text_to_binary(args.encode)
+        dolphin = binary_to_dolphin(binary)
+        print(f"Dolphin: {dolphin}")
+        print(binary_to_text(decode_dolphin(binary)))
+        if args.copy:
+            copy_to_clipboard(dolphin)
+        if args.speak:
+            speak_dolphin(dolphin)
+    elif args.decode:
+        binary = decode_dolphin(args.decode)
+        text = binary_to_text(binary)
+        print(f"Binary: {binary}")
+        print(f"Text: {text}")
+        if args.copy:
+            copy_to_clipboard(text)
+    # if no args were supplied, prompt the user
+    elif not any(vars(args).values()):
+        # ask them if they want to encode or decode
+        print("Encode or decode? (e/d):")
+        encode_or_decode = input()
+        if encode_or_decode == "e":
+            print("Text to encode:")
+            text = input()
+            binary = text_to_binary(text)
+            dolphin = binary_to_dolphin(binary)
+            print(f"Dolphin: {dolphin}")
+            print("Copy to clipboard? (y/n):")
+            copy = input()
+            if copy == "y":
+                copy_to_clipboard(dolphin)
+            print("Speak dolphin? (y/n):")
+            speak = input()
+            if speak == "y":
+                speak_dolphin(dolphin)
+        elif encode_or_decode == "d":
+            print("Dolphin to decode:")
+            dolphin = input()
+            binary = decode_dolphin(dolphin)
+            text = binary_to_text(binary)
+            print(f"Binary: {binary}")
+            print(f"Text: {text}")
+
+
+def print_ascii_dolphin():
+    """Print the ASCII dolphin."""
+    print(
+        """
+      _____ _____ _____ _____ _______       _        
+     |  __ \_   _/ ____|_   _|__   __|/\   | |       
+     | |  | || || |  __  | |    | |  /  \  | |       
+     | |  | || || | |_ | | |    | | / /\ \ | |       
+     | |__| || || |__| |_| |_   | |/ ____ \| |____   
+     |_____/_____\_____|_____|__|_/_/  _ \_\______|_ 
+     
+         |  __ \ / __ \| |    |  __ \| |  | |_   _| \ | |
+         | |  | | |  | | |    | |__) | |__| | | | |  \| |
+         | |  | | |  | | |    |  ___/|  __  | | | | . ` |
+         | |__| | |__| | |____| |    | |  | |_| |_| |\  |
+         |_____/ \____/|______|_|    |_|  |_|_____|_| \_|
+                                                 
+                                                 
+                                           00001110
+                                       11110110 11110110
+                                          00001110
+                               _.-~  )  00100
+                    _..--~~~~,'   ,-/  00  _
+                 .-'. . . .'   ,-','  1 ,' )
+               ,'. . . _   ,--~,-'__..-'  ,'
+             ,'. . .  (@)' ---~~~~      ,'
+            /. . . . '~~             ,-'
+           /. . . . .             ,-'
+          ; . . . .  - .        ,'
+         : . . . .       _     /
+        . . . . .          `-.:
+       . . . ./  - .          )
+      .  . . |  _____..-~^-._/ ____.--~^--._
+~---~~~~----~~~~             ~~
+    """
+    )
+
+
 def run():
     """Run the program."""
-    text = input("Enter text to convert to dolphin: ")
-    binary = text_to_binary(text)
-    dolphin = binary_to_dolphin(binary)
-    print(f"Dolphin: {dolphin}")
-    print(binary_to_text(decode_dolphin(binary)))
-    copy = input("Copy to clipboard? (y/n): (pip install pyperclip)")
-    if copy.lower() == "y":
-        copy_to_clipboard(dolphin)
-    speak = input(
-        "Speak dolphin? (y/n): (pip install pydub pyrubberband soundfile gtts)"
-    )
-    if speak.lower() == "y":
-        speak_dolphin(dolphin)
+    print_ascii_dolphin()
+    command_line_interface()
 
 
 if __name__ == "__main__":
